e42587898b285dc997cff5f24151f85dcebc1e92
Anthony COUDENE <anthony.coudene@technidata-web.com>
2023-01-30 18:59:07 +0100

    Finalize example

--- MauiBlazorApp/Cryptography/DeviceKeyGenerator.cs
+++ MauiBlazorApp/Cryptography/DeviceKeyGenerator.cs
@@ -1,12 +1,31 @@
 ﻿using CryptographyProvider;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
 
-namespace MauiBlazorApp.Cryptography
+namespace MauiBlazorApp.Cryptography;
+public class DeviceKeyGenerator : IStatefulKeyGenerator
 {
-  public class DeviceKeyGenerator : IStatefulKeyGenerator
+  public string GenerateKey(byte keySizeInBytes)
   {
-    public string GenerateKey(byte keySizeInBytes)
-    {
-      throw new NotImplementedException();
-    }
+    var currentDeviceInfo = DeviceInfo.Current;
+    string serializedData = JsonConvert.SerializeObject(currentDeviceInfo);
+    string hash = ComputeHash(serializedData, keySizeInBytes);
+    return hash;
+  }
+
+  public static String ComputeHash(string value, byte keySizeInBytes)
+  {
+    StringBuilder Sb = new StringBuilder();
+
+    using var hash = SHA256.Create();
+
+    Encoding enc = Encoding.UTF8;
+    byte[] result = hash.ComputeHash(enc.GetBytes(value), 0, keySizeInBytes);
+
+    foreach (byte b in result)
+      Sb.Append(b.ToString("x2"));
+
+    return Sb.ToString(0, keySizeInBytes);
   }
 }
