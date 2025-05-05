33dc615796c2d4f6b0d063cc209bf2ee55328bc6
Anthony COUDENE <anthony.coudene@technidata-web.com>
2025-05-05 17:26:43 +0200

    Upgrade to .Net 8 and MAUI 9

---
+++ MauiBlazorHybridApp/Cryptography/DeviceKeyGenerator.cs
@@ -0,0 +1,31 @@
+﻿using CryptographyProvider;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace MauiBlazorHybridApp.Cryptography;
+public class DeviceKeyGenerator : IStatefulKeyGenerator
+{
+  public string GenerateKey(byte keySizeInBytes)
+  {
+    var currentDeviceInfo = DeviceInfo.Current;
+    string serializedData = JsonConvert.SerializeObject(currentDeviceInfo);
+    string hash = ComputeHash(serializedData, keySizeInBytes);
+    return hash;
+  }
+
+  public static String ComputeHash(string value, byte keySizeInBytes)
+  {
+    StringBuilder Sb = new StringBuilder();
+
+    using var hash = SHA256.Create();
+
+    Encoding enc = Encoding.UTF8;
+    byte[] result = hash.ComputeHash(enc.GetBytes(value), 0, keySizeInBytes);
+
+    foreach (byte b in result)
+      Sb.Append(b.ToString("x2"));
+
+    return Sb.ToString(0, keySizeInBytes);
+  }
+}
---
+++ MauiBlazorHybridApp/MauiProgram.cs
@@ -0,0 +1,33 @@
+﻿using CryptographyProvider;
+using MauiBlazorHybridApp.Cryptography;
+using Microsoft.Extensions.Logging;
+
+namespace MauiBlazorHybridApp
+{
+  public static class MauiProgram
+  {
+    public static MauiApp CreateMauiApp()
+    {
+      var builder = MauiApp.CreateBuilder();
+      builder
+        .UseMauiApp<App>()
+        .ConfigureFonts(fonts =>
+        {
+          fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+        });
+
+      builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+  		builder.Services.AddBlazorWebViewDeveloperTools();
+  		builder.Logging.AddDebug();
+#endif
+      
+      builder.Services.AddSingleton<ICryptographyProvider, AeSCryptographyProvider>();
+      builder.Services.AddSingleton<IStatefulKeyGenerator, DeviceKeyGenerator>();
+      builder.Services.AddSingleton<IStatefulCryptographyProvider, StatefulCryptographyProvider>();
+
+      return builder.Build();
+    }
+  }
+}
